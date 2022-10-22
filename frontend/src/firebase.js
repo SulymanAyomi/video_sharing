1f0d58a60de8051763fe1984eb694b08d46ebd02
Sulyman Ayomi <Sulymanayomi@gmail.com>
2022-10-22 15:28:09 +0100

    change II

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX5x3wvWim4EBsNE73rsnkhKp74Ejw7V4",
+  apiKey: process.env.REACT_APP_FIRESTORE_API,
   authDomain: "ourtube-a2b99.firebaseapp.com",
   projectId: "ourtube-a2b99",
   storageBucket: "ourtube-a2b99.appspot.com",
   messagingSenderId: "1027937944652",
-  appId: "1:1027937944652:web:c3adf908b997766da820e9",
+  appId: process.env.REACT_APP_FIRESTORE_APPID,
   measurementId: "G-E9S49EXYD4",
 };
-console.log(process.env.FIREBASE_API, "jjjj");
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
