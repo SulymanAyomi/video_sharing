56383db4dd36b14fa3bd51240ee6d88b99912ca7
Sulyman Ayomi <Sulymanayomi@gmail.com>
2022-10-26 17:01:53 +0100

    new

--- frontend/src/components/Notfound.jsx
+++ frontend/src/components/Notfound.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const No = styled.div`
+  text-align: center;
+  color: ${({ theme }) => theme.text};
+`;
 export default function NotFound() {
   return (
-    <div>
+    <No>
       <h1>Oops! You seem to be lost.</h1>
       <p>Here are some helpful links:</p>
       <Link to="/">Home</Link>
-    </div>
+    </No>
   );
 }
