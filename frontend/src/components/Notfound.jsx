aab6a47daa590dab6861e995f6fc1d302b50a5d3
Sulyman Ayomi <Sulymanayomi@gmail.com>
2022-10-30 08:29:54 +0000

    fixed router

--- frontend/src/components/Notfound.jsx
+++ frontend/src/components/Notfound.jsx
@@ -5,11 +5,15 @@ const No = styled.div`
   text-align: center;
   color: ${({ theme }) => theme.text};
 `;
+const H1 = styled.h1`
+  font-size: 20px;
+  margin: 15px 0;
+`;
 export default function NotFound() {
   return (
     <No>
-      <h1>Oops! You seem to be lost.</h1>
-      <p>Here are some helpful links:</p>
+      <H1>Oops! You seem to be lost.</H1>
+      <p style={{margin: "15px 0"}}>Here are some helpful links:</p>
       <Link to="/">Home</Link>
     </No>
   );
